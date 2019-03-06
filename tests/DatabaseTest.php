86ecb04ae44cc03cbded4db470db94a40bc13685
Natan Felles <natanfelles@gmail.com>
2019-03-06 16:52:35 -0300

    Test Database

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -15,11 +15,34 @@ class DatabaseTest extends TestCase
 		$this->database = new Database();
 	}
 
-	public function testSample()
+	public function testProtectIdentifier()
 	{
-		$this->assertEquals(
-			'Framework\Database\Database::test',
-			$this->database->test()
-		);
+		$this->assertEquals('`foo`', $this->database->protectIdentifier('foo'));
+		$this->assertEquals('```foo```', $this->database->protectIdentifier('`foo`'));
+		$this->assertEquals('`foo ``bar`', $this->database->protectIdentifier('foo `bar'));
+		$this->assertEquals('`foo`.`bar`', $this->database->protectIdentifier('foo.bar'));
+		$this->assertEquals('`foo`.*', $this->database->protectIdentifier('foo.*'));
+		$this->assertEquals('```foo```.*', $this->database->protectIdentifier('`foo`.*'));
+		$this->assertEquals('`db`.`table`.*', $this->database->protectIdentifier('db.table.*'));
+	}
+
+	public function testQuote()
+	{
+		$this->assertEquals(0, $this->database->quote(0));
+		$this->assertEquals(1, $this->database->quote(1));
+		$this->assertEquals(-1, $this->database->quote(-1));
+		$this->assertEquals(.0, $this->database->quote(.0));
+		$this->assertEquals(1.1, $this->database->quote(1.1));
+		$this->assertEquals(-1.1, $this->database->quote(-1.1));
+		$this->assertEquals("'0'", $this->database->quote('0'));
+		$this->assertEquals("'-1'", $this->database->quote('-1'));
+		$this->assertEquals("'abc'", $this->database->quote('abc'));
+		$this->assertEquals("'ab\\'c'", $this->database->quote("ab'c"));
+		$this->assertEquals("'ab\\'cd\\'\\''", $this->database->quote("ab'cd''"));
+		$this->assertEquals('\'ab\"cd\"\"\'', $this->database->quote('ab"cd""'));
+		$this->assertEquals('NULL', $this->database->quote(null));
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Invalid value type: array');
+		$this->database->quote([]);
 	}
 }
