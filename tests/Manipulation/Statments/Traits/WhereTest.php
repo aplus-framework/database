84220fc1547ddf06e6f256f568beee7bef0666ba
Natan Felles <natanfelles@gmail.com>
2019-03-06 19:04:03 -0300

    Test Where Operators

--- tests/Manipulation/Statments/Traits/WhereTest.php
+++ tests/Manipulation/Statments/Traits/WhereTest.php
@@ -54,7 +54,7 @@ class WhereTest extends TestCase
 		);
 	}
 
-	public function testWhereLike()
+	public function testLike()
 	{
 		$this->statement->whereLike('email', '%@mail.com');
 		$this->assertEquals(" WHERE `email` LIKE '%@mail.com'", $this->statement->render());
@@ -74,7 +74,7 @@ class WhereTest extends TestCase
 		);
 	}
 
-	public function testWhereNotLike()
+	public function testNotLike()
 	{
 		$this->statement->whereNotLike('email', '%@mail.com');
 		$this->assertEquals(" WHERE `email` NOT LIKE '%@mail.com'", $this->statement->render());
@@ -92,5 +92,49 @@ class WhereTest extends TestCase
 			" WHERE `email` NOT LIKE '%@mail.com' OR `name` NOT LIKE 'foo%' AND (id) NOT LIKE (10)",
 			$this->statement->render()
 		);
+	}
+
+	public function testOperators()
+	{
+		$this->statement
+			->where('=', '=', 1)
+			->orWhere('<=>', '<=>', 1)
+			->where('!=', '!=', 1)
+			->orWhere('<>', '<>', 1)
+			->where('>', '>', 1)
+			->orWhere('>=', '>=', 1)
+			->where('<', '<', 1)
+			->orWhere('<=', '<=', 1)
+			->where('like', 'Like', 1)
+			->orWhere('not like', 'Not Like', 1)
+			->where('in', 'In', 1)
+			->orWhere('not in', 'Not In', 1)
+			->where('between', 'Between', 1, 5)
+			->orWhere('not between', 'Not Between', 1, 5)
+			->where('is null', 'Is Null')
+			->orWhere('is not null', 'Is Not Null');
+		$this->assertEquals(
+			' WHERE `=` = 1'
+			. ' OR `<=>` <=> 1'
+			. ' AND `!=` != 1'
+			. ' OR `<>` <> 1'
+			. ' AND `>` > 1'
+			. ' OR `>=` >= 1'
+			. ' AND `<` < 1'
+			. ' OR `<=` <= 1'
+			. ' AND `like` LIKE 1'
+			. ' OR `not like` NOT LIKE 1'
+			. ' AND `in` IN (1)'
+			. ' OR `not in` NOT IN (1)'
+			. ' AND `between` BETWEEN 1 AND 5'
+			. ' OR `not between` NOT BETWEEN 1 AND 5'
+			. ' AND `is null` IS NULL'
+			. ' OR `is not null` IS NOT NULL',
+			$this->statement->render()
+		);
+		$this->statement->where('any', 'foo');
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Invalid comparison operator: foo');
+		$this->statement->render();
 	}
 }
