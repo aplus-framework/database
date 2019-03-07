3b638854186bb87e84cffb59a7947084464b568a
Natan Felles <natanfelles@gmail.com>
2019-03-07 14:32:33 -0300

    Add PHP doc comments on Database

--- src/Database.php
+++ src/Database.php
@@ -1,14 +1,35 @@
 <?php namespace Framework\Database;
 
+/**
+ * Class Database.
+ */
 class Database
 {
+	/**
+	 * @var \PDO|null
+	 */
 	protected $pdo;
 
+	/**
+	 * Database constructor.
+	 *
+	 * @param \PDO|null $pdo
+	 */
 	public function __construct(\PDO $pdo = null)
 	{
 		$this->pdo = $pdo;
 	}
 
+	/**
+	 * Protect identifiers.
+	 *
+	 * @param string $identifier Table, column, database name or a fully-qualified identifier
+	 *                           separated by dots
+	 *
+	 * @see https://mariadb.com/kb/en/library/identifier-qualifiers/
+	 *
+	 * @return string
+	 */
 	public function protectIdentifier(string $identifier) : string
 	{
 		$identifier = \strtr($identifier, ['`' => '``', '.' => '`.`']);
@@ -16,6 +37,19 @@ class Database
 		return \strtr($identifier, ['`*`' => '*']);
 	}
 
+	/**
+	 * Quote SQL values.
+	 *
+	 * @param float|int|string|null $value Value to be quoted
+	 *
+	 * @see https://mariadb.com/kb/en/library/quote/
+	 *
+	 * @throws \InvalidArgumentException For invalid value type
+	 *
+	 * @return float|int|string If the value is null, returns a string containing the word "NULL".
+	 *                          If is a string, returns the quoted string. The types int or float
+	 *                          returns the same input value.
+	 */
 	public function quote($value)
 	{
 		if (\is_string($value)) {
