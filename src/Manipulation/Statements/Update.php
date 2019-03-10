d8f955a0ad099da13f3cb85506656056aa5fd0af
Natan Felles <natanfelles@gmail.com>
2019-03-10 10:56:35 -0300

    Add Update Statement

---
+++ src/Manipulation/Statements/Update.php
@@ -0,0 +1,98 @@
+<?php namespace Framework\Database\Manipulation\Statements;
+
+/**
+ * Class Update.
+ *
+ * @see https://mariadb.com/kb/en/library/update/
+ */
+class Update extends Statement
+{
+	use Traits\Set;
+	use Traits\Where;
+	use Traits\OrderBy;
+	/**
+	 * Convert errors to warnings, which will not stop inserts of additional rows.
+	 *
+	 * @see https://mariadb.com/kb/en/library/insert-ignore/
+	 */
+	public const OPT_IGNORE = 'IGNORE';
+	/**
+	 * @see https://mariadb.com/kb/en/library/high_priority-and-low_priority/
+	 */
+	public const OPT_LOW_PRIORITY = 'LOW_PRIORITY';
+
+	protected function renderOptions() : ?string
+	{
+		if ( ! isset($this->sql['options'])) {
+			return null;
+		}
+		$options = $this->sql['options'];
+		foreach ($options as &$option) {
+			$input = $option;
+			$option = \strtoupper($option);
+			if ( ! \in_array($option, [
+				static::OPT_IGNORE,
+				static::OPT_LOW_PRIORITY,
+			], true)) {
+				throw new \InvalidArgumentException("Invalid option: {$input}");
+			}
+		}
+		unset($option);
+		$options = \implode(' ', $options);
+		return " {$options}";
+	}
+
+	public function table(...$references)
+	{
+		foreach ($references as $reference) {
+			$this->sql['table'][] = $reference;
+		}
+		return $this;
+	}
+
+	protected function renderTable() : ?string
+	{
+		if ( ! isset($this->sql['table'])) {
+			return null;
+		}
+		$tables = [];
+		foreach ($this->sql['table'] as $table) {
+			$tables[] = $this->renderAliasedIdentifier($table);
+		}
+		return ' ' . \implode(', ', $tables);
+	}
+
+	public function limit(int $limit)
+	{
+		return $this->setLimit($limit);
+	}
+
+	public function sql() : string
+	{
+		$sql = 'UPDATE' . \PHP_EOL;
+		if ($part = $this->renderOptions()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		if ($part = $this->renderTable()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		if ($part = $this->renderSet()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		if ($part = $this->renderWhere()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		if ($part = $this->renderOrderBy()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		if ($part = $this->renderLimit()) {
+			$sql .= $part . \PHP_EOL;
+		}
+		return $sql;
+	}
+
+	public function run()
+	{
+		return $this->manipulation->database->pdo->exec($this->sql());
+	}
+}
