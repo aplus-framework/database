15a578af354ac612b714fe25b620f092424f356c
Natan Felles <natanfelles@gmail.com>
2019-03-16 10:34:55 -0300

    Test ForeignKey Index

---
+++ tests/Definition/Indexes/Keys/ForeignKeyTest.php
@@ -0,0 +1,59 @@
+<?php namespace Tests\Database\Definition\Indexes\Keys;
+
+use Framework\Database\Definition\Indexes\Keys\ForeignKey;
+use Tests\Database\TestCase;
+
+class ForeignKeyTest extends TestCase
+{
+	/**
+	 * @var ForeignKey
+	 */
+	protected $index;
+
+	protected function setUp()
+	{
+		$this->index = new ForeignKey($this->database, 'user_id');
+	}
+
+	public function testEmptyReferences()
+	{
+		$this->expectException(\LogicException::class);
+		$this->expectExceptionMessage('REFERENCES clause was not set');
+		$this->index->sql();
+	}
+
+	public function testReferences()
+	{
+		$this->index->references('users', 'id');
+		$this->assertEquals(
+			' FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)',
+			$this->index->sql()
+		);
+	}
+
+	public function testOnDelete()
+	{
+		$this->index->references('users', 'id')->onDelete('restrict');
+		$this->assertEquals(
+			' FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT',
+			$this->index->sql()
+		);
+	}
+
+	public function testOnUpdate()
+	{
+		$this->index->references('users', 'id')->onUpdate('cascade');
+		$this->assertEquals(
+			' FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE',
+			$this->index->sql()
+		);
+	}
+
+	public function testInvalidReferenceOption()
+	{
+		$this->index->references('users', 'id')->onUpdate('foo');
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Invalid reference option: foo');
+		$this->index->sql();
+	}
+}
