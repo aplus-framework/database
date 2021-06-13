bd79994deb3fc17e30f78bf518498cb731696336
Natan Felles <natanfelles@gmail.com>
2021-06-13 08:04:50 -0300

    Result Field magic __get returns mixed

--- src/Result/Field.php
+++ src/Result/Field.php
@@ -81,7 +81,7 @@ class Field
 		$this->setFlags();
 	}
 
-	public function __get(string $name)
+	public function __get(string $name) : mixed
 	{
 		if (\property_exists($this, $name)) {
 			return $this->{$name};
@@ -105,7 +105,7 @@ class Field
 			\MYSQLI_TYPE_FLOAT => 'float',
 			\MYSQLI_TYPE_GEOMETRY => 'geometry',
 			\MYSQLI_TYPE_INT24 => 'int24',
-			\MYSQLI_TYPE_INTERVAL => 'interval',
+			\MYSQLI_TYPE_INTERVAL => 'interval', // @phpstan-ignore-line
 			\MYSQLI_TYPE_JSON => 'json',
 			\MYSQLI_TYPE_LONG => 'long',
 			\MYSQLI_TYPE_LONG_BLOB => 'long_blob',
@@ -119,7 +119,7 @@ class Field
 			\MYSQLI_TYPE_STRING => 'string',
 			\MYSQLI_TYPE_TIME => 'time',
 			\MYSQLI_TYPE_TIMESTAMP => 'timestamp',
-			\MYSQLI_TYPE_TINY => 'tiny',
+			\MYSQLI_TYPE_TINY => 'tiny', // @phpstan-ignore-line
 			\MYSQLI_TYPE_TINY_BLOB => 'tiny_blob',
 			\MYSQLI_TYPE_VAR_STRING => 'var_string',
 			\MYSQLI_TYPE_YEAR => 'year',
