d5492e2b21d4bbd43528ed671d57eb47e58c6ca8
Natan Felles <natanfelles@gmail.com>
2021-12-18 22:53:23 -0300

    Add charsets to code completion

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -10,6 +10,80 @@
 namespace PHPSTORM_META;
 
 registerArgumentsSet(
+    'charsets',
+    'armscii8',
+    'ascii',
+    'big5',
+    'binary',
+    'cp1250',
+    'cp1251',
+    'cp1256',
+    'cp1257',
+    'cp850',
+    'cp852',
+    'cp866',
+    'cp932',
+    'dec8',
+    'eucjpms',
+    'euckr',
+    'gb2312',
+    'gbk',
+    'geostd8',
+    'greek',
+    'hebrew',
+    'hp8',
+    'keybcs2',
+    'koi8r',
+    'koi8u',
+    'latin1',
+    'latin2',
+    'latin5',
+    'latin7',
+    'macce',
+    'macroman',
+    'sjis',
+    'swe7',
+    'tis620',
+    'ucs2',
+    'ujis',
+    'utf16',
+    'utf16le',
+    'utf32',
+    'utf8',
+    'utf8mb3',
+    'utf8mb4',
+);
+expectedArguments(
+    \Framework\Database\Database::setCollations(),
+    0,
+    argumentsSet('charsets')
+);
+expectedArguments(
+    \Framework\Database\Definition\AlterSchema::charset(),
+    0,
+    argumentsSet('charsets')
+);
+expectedArguments(
+    \Framework\Database\Definition\CreateSchema::charset(),
+    0,
+    argumentsSet('charsets')
+);
+expectedArguments(
+    \Framework\Database\Definition\Table\Columns\String\StringDataType::charset(),
+    0,
+    argumentsSet('charsets')
+);
+expectedArguments(
+    \Framework\Database\Manipulation\LoadData::charset(),
+    0,
+    argumentsSet('charsets')
+);
+expectedArguments(
+    \Framework\Database\Manipulation\Select::intoOutfile(),
+    1,
+    argumentsSet('charsets')
+);
+registerArgumentsSet(
     'manipulation_delete_opt',
     \Framework\Database\Manipulation\Delete::OPT_IGNORE,
     \Framework\Database\Manipulation\Delete::OPT_LOW_PRIORITY,
