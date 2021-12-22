9f2b2f161749b86a5e5c2eead0b744e81d06df48
Natan Felles <natanfelles@gmail.com>
2021-12-22 20:02:58 -0300

    Update ALTER TABLE

--- src/Definition/AlterTable.php
+++ src/Definition/AlterTable.php
@@ -24,6 +24,43 @@ use LogicException;
 class AlterTable extends TableStatement
 {
     /**
+     * @var string
+     */
+    public const ALGO_COPY = 'COPY';
+    /**
+     * @var string
+     */
+    public const ALGO_DEFAULT = 'DEFAULT';
+    /**
+     * @var string
+     */
+    public const ALGO_INPLACE = 'INPLACE';
+    /**
+     * @var string
+     */
+    public const ALGO_INSTANT = 'INSTANT';
+    /**
+     * @var string
+     */
+    public const ALGO_NOCOPY = 'NOCOPY';
+    /**
+     * @var string
+     */
+    public const LOCK_DEFAULT = 'DEFAULT';
+    /**
+     * @var string
+     */
+    public const LOCK_EXCLUSIVE = 'EXCLUSIVE';
+    /**
+     * @var string
+     */
+    public const LOCK_NONE = 'NONE';
+    /**
+     * @var string
+     */
+    public const LOCK_SHARED = 'SHARED';
+
+    /**
      * @return static
      */
     public function online() : static
@@ -55,6 +92,20 @@ class AlterTable extends TableStatement
             return null;
         }
         return ' IGNORE';
+    }
+
+    public function ifExists() : static
+    {
+        $this->sql['if_exists'] = true;
+        return $this;
+    }
+
+    protected function renderIfExists() : ?string
+    {
+        if ( ! isset($this->sql['if_exists'])) {
+            return null;
+        }
+        return ' IF EXISTS';
     }
 
     /**
@@ -98,6 +149,23 @@ class AlterTable extends TableStatement
             );
         }
         return " WAIT {$this->sql['wait']}";
+    }
+
+    public function noWait() : static
+    {
+        $this->sql['no_wait'] = true;
+        return $this;
+    }
+
+    protected function renderNoWait() : ?string
+    {
+        if ( ! isset($this->sql['no_wait'])) {
+            return null;
+        }
+        if (isset($this->sql['wait'])) {
+            throw new LogicException('WAIT and NOWAIT can not be used together');
+        }
+        return ' NOWAIT';
     }
 
     /**
@@ -163,62 +231,375 @@ class AlterTable extends TableStatement
         return $definition->sql('MODIFY');
     }
 
-    /**
-     * @param callable $definition
-     *
-     * @return static
-     */
-    public function dropColumns(callable $definition) : static
+    public function dropColumn(string $name, bool $ifExists = false) : static
     {
-        $this->sql['drop_columns'] = $definition;
+        $this->sql['drop_columns'][$name] = $ifExists;
         return $this;
     }
 
+    protected function renderDropColumns() : ?string
+    {
+        if ( ! isset($this->sql['drop_columns'])) {
+            return null;
+        }
+        $drops = [];
+        foreach ($this->sql['drop_columns'] as $name => $ifExists) {
+            $name = $this->database->protectIdentifier($name);
+            $ifExists = $ifExists ? 'IF EXISTS ' : '';
+            $drops[] = ' DROP COLUMN ' . $ifExists . $name;
+        }
+        return \implode(',' . \PHP_EOL, $drops);
+    }
+
+    public function dropPrimaryKey() : static
+    {
+        $this->sql['drop_primary_key'] = true;
+        return $this;
+    }
+
+    protected function renderDropPrimaryKey() : ?string
+    {
+        if ( ! isset($this->sql['drop_primary_key'])) {
+            return null;
+        }
+        return ' DROP PRIMARY KEY';
+    }
+
+    public function dropKey(string $name, bool $ifExists = false) : static
+    {
+        $this->sql['drop_keys'][$name] = $ifExists;
+        return $this;
+    }
+
+    protected function renderDropKeys() : ?string
+    {
+        if ( ! isset($this->sql['drop_keys'])) {
+            return null;
+        }
+        $drops = [];
+        foreach ($this->sql['drop_keys'] as $name => $ifExists) {
+            $name = $this->database->protectIdentifier($name);
+            $ifExists = $ifExists ? 'IF EXISTS ' : '';
+            $drops[] = ' DROP KEY ' . $ifExists . $name;
+        }
+        return \implode(',' . \PHP_EOL, $drops);
+    }
+
+    public function dropForeignKey(string $name, bool $ifExists = false) : static
+    {
+        $this->sql['drop_foreign_keys'][$name] = $ifExists;
+        return $this;
+    }
+
+    protected function renderDropForeignKeys() : ?string
+    {
+        if ( ! isset($this->sql['drop_foreign_keys'])) {
+            return null;
+        }
+        $drops = [];
+        foreach ($this->sql['drop_foreign_keys'] as $name => $ifExists) {
+            $name = $this->database->protectIdentifier($name);
+            $ifExists = $ifExists ? 'IF EXISTS ' : '';
+            $drops[] = ' DROP FOREIGN KEY ' . $ifExists . $name;
+        }
+        return \implode(',' . \PHP_EOL, $drops);
+    }
+
+    public function dropConstraint(string $name, bool $ifExists = false) : static
+    {
+        $this->sql['drop_constraints'][$name] = $ifExists;
+        return $this;
+    }
+
+    protected function renderDropConstraints() : ?string
+    {
+        if ( ! isset($this->sql['drop_constraints'])) {
+            return null;
+        }
+        $drops = [];
+        foreach ($this->sql['drop_constraints'] as $name => $ifExists) {
+            $name = $this->database->protectIdentifier($name);
+            $ifExists = $ifExists ? 'IF EXISTS ' : '';
+            $drops[] = ' DROP CONSTRAINT ' . $ifExists . $name;
+        }
+        return \implode(',' . \PHP_EOL, $drops);
+    }
+
+    public function disableKeys() : static
+    {
+        $this->sql['disable_keys'] = true;
+        return $this;
+    }
+
+    protected function renderDisableKeys() : ?string
+    {
+        if ( ! isset($this->sql['disable_keys'])) {
+            return null;
+        }
+        return ' DISABLE KEYS';
+    }
+
+    public function enableKeys() : static
+    {
+        $this->sql['enable_keys'] = true;
+        return $this;
+    }
+
+    protected function renderEnableKeys() : ?string
+    {
+        if ( ! isset($this->sql['enable_keys'])) {
+            return null;
+        }
+        return ' ENABLE KEYS';
+    }
+
+    public function renameTo(string $newTableName) : static
+    {
+        $this->sql['rename_to'] = $newTableName;
+        return $this;
+    }
+
+    protected function renderRenameTo() : ?string
+    {
+        if ( ! isset($this->sql['rename_to'])) {
+            return null;
+        }
+        return ' RENAME TO ' . $this->database->protectIdentifier($this->sql['rename_to']);
+    }
+
+    public function renameColumn(string $name, string $newName) : static
+    {
+        $this->sql['rename_columns'][$name] = $newName;
+        return $this;
+    }
+
+    protected function renderRenameColumns() : ?string
+    {
+        if ( ! isset($this->sql['rename_columns'])) {
+            return null;
+        }
+        $renames = [];
+        foreach ($this->sql['rename_columns'] as $name => $newName) {
+            $name = $this->database->protectIdentifier($name);
+            $newName = $this->database->protectIdentifier($newName);
+            $renames[] = ' RENAME COLUMN ' . $name . ' TO ' . $newName;
+        }
+        return \implode(',' . \PHP_EOL, $renames);
+    }
+
+    public function renameKey(string $name, string $newName) : static
+    {
+        $this->sql['rename_keys'][$name] = $newName;
+        return $this;
+    }
+
+    protected function renderRenameKeys() : ?string
+    {
+        if ( ! isset($this->sql['rename_keys'])) {
+            return null;
+        }
+        $renames = [];
+        foreach ($this->sql['rename_keys'] as $name => $newName) {
+            $name = $this->database->protectIdentifier($name);
+            $newName = $this->database->protectIdentifier($newName);
+            $renames[] = ' RENAME KEY ' . $name . ' TO ' . $newName;
+        }
+        return \implode(',' . \PHP_EOL, $renames);
+    }
+
+    public function convertToCharset(string $charset, string $collation = null) : static
+    {
+        $this->sql['convert_to_charset'] = [
+            'charset' => $charset,
+            'collation' => $collation,
+        ];
+        return $this;
+    }
+
+    protected function renderConvertToCharset() : ?string
+    {
+        if ( ! isset($this->sql['convert_to_charset'])) {
+            return null;
+        }
+        $charset = $this->database->quote($this->sql['convert_to_charset']['charset']);
+        $convert = ' CONVERT TO CHARACTER SET ' . $charset;
+        if (isset($this->sql['convert_to_charset']['collation'])) {
+            $convert .= ' COLLATE ' . $this->database->quote($this->sql['convert_to_charset']['collation']);
+        }
+        return $convert;
+    }
+
+    public function charset(?string $charset) : static
+    {
+        $this->sql['charset'] = $charset ?? 'DEFAULT';
+        return $this;
+    }
+
+    protected function renderCharset() : ?string
+    {
+        if ( ! isset($this->sql['charset'])) {
+            return null;
+        }
+        $charset = \strtolower($this->sql['charset']);
+        if ($charset === 'default') {
+            return ' DEFAULT CHARACTER SET';
+        }
+        return ' CHARACTER SET = ' . $this->database->quote($charset);
+    }
+
+    public function collate(?string $collation) : static
+    {
+        $this->sql['collate'] = $collation ?? 'DEFAULT';
+        return $this;
+    }
+
+    protected function renderCollate() : ?string
+    {
+        if ( ! isset($this->sql['collate'])) {
+            return null;
+        }
+        $collate = \strtolower($this->sql['collate']);
+        if ($collate === 'default') {
+            return ' DEFAULT COLLATE';
+        }
+        return ' COLLATE = ' . $this->database->quote($collate);
+    }
+
     /**
-     * @param callable $definition
+     * @param string $type
+     *
+     * @see https://mariadb.com/kb/en/alter-table/#lock
+     * @see AlterTable::LOCK_DEFAULT
+     * @see AlterTable::LOCK_EXCLUSIVE
+     * @see AlterTable::LOCK_NONE
+     * @see AlterTable::LOCK_SHARED
      *
      * @return static
      */
-    public function drop(callable $definition) : static
+    public function lock(string $type) : static
     {
-        $this->sql['drop'] = $definition;
+        $this->sql['lock'] = $type;
         return $this;
+    }
+
+    protected function renderLock() : ?string
+    {
+        if ( ! isset($this->sql['lock'])) {
+            return null;
+        }
+        $lock = \strtoupper($this->sql['lock']);
+        if ( ! \in_array($lock, [
+            static::LOCK_DEFAULT,
+            static::LOCK_EXCLUSIVE,
+            static::LOCK_NONE,
+            static::LOCK_SHARED,
+        ], true)) {
+            throw new InvalidArgumentException("Invalid LOCK value: {$this->sql['lock']}");
+        }
+        return ' LOCK = ' . $lock;
+    }
+
+    public function force() : static
+    {
+        $this->sql['force'] = true;
+        return $this;
+    }
+
+    protected function renderForce() : ?string
+    {
+        if ( ! isset($this->sql['force'])) {
+            return null;
+        }
+        return ' FORCE';
+    }
+
+    /**
+     * @param string $algo
+     *
+     * @see https://mariadb.com/kb/en/innodb-online-ddl-overview/#algorithm
+     * @see AlterTable::ALGO_COPY
+     * @see AlterTable::ALGO_DEFAULT
+     * @see AlterTable::ALGO_INPLACE
+     * @see AlterTable::ALGO_INSTANT
+     * @see AlterTable::ALGO_NOCOPY
+     *
+     * @return static
+     */
+    public function algorithm(string $algo) : static
+    {
+        $this->sql['algorithm'] = $algo;
+        return $this;
+    }
+
+    protected function renderAlgorithm() : ?string
+    {
+        if ( ! isset($this->sql['algorithm'])) {
+            return null;
+        }
+        $algo = \strtoupper($this->sql['algorithm']);
+        if ( ! \in_array($algo, [
+            static::ALGO_COPY,
+            static::ALGO_DEFAULT,
+            static::ALGO_INPLACE,
+            static::ALGO_INSTANT,
+            static::ALGO_NOCOPY,
+        ], true)) {
+            throw new InvalidArgumentException("Invalid ALGORITHM value: {$this->sql['algorithm']}");
+        }
+        return ' ALGORITHM = ' . $algo;
     }
 
     public function sql() : string
     {
         $sql = 'ALTER' . $this->renderOnline() . $this->renderIgnore();
-        $sql .= ' TABLE';
+        $sql .= ' TABLE' . $this->renderIfExists();
         $sql .= $this->renderTable() . \PHP_EOL;
-        $part = $this->renderWait();
+        $part = $this->renderWait() . $this->renderNoWait();
         if ($part) {
             $sql .= $part . \PHP_EOL;
         }
-        $part = $this->renderOptions();
-        $before = $part;
-        if ($part) {
-            $sql .= $part;
-        }
-        $part = $this->renderAdd();
-        if ($part) {
-            $sql .= $this->beforePart($before) . $part;
-            $before = $part;
-        }
-        $part = $this->renderChange();
-        if ($part) {
-            $sql .= $this->beforePart($before) . $part;
-            $before = $part;
-        }
-        $part = $this->renderModify();
-        if ($part) {
-            $sql .= $this->beforePart($before) . $part;
-        }
+        $sql .= $this->joinParts([
+            $this->renderOptions(),
+            $this->renderAdd(),
+            $this->renderChange(),
+            $this->renderModify(),
+            $this->renderDropColumns(),
+            $this->renderDropPrimaryKey(),
+            $this->renderDropKeys(),
+            $this->renderDropForeignKeys(),
+            $this->renderDropConstraints(),
+            $this->renderDisableKeys(),
+            $this->renderEnableKeys(),
+            $this->renderRenameTo(),
+            $this->renderRenameColumns(),
+            $this->renderRenameKeys(),
+            $this->renderConvertToCharset(),
+            $this->renderCharset(),
+            $this->renderCollate(),
+            $this->renderAlgorithm(),
+            $this->renderLock(),
+            $this->renderForce(),
+        ]);
         return $sql;
     }
 
-    protected function beforePart(?string $before) : string
+    /**
+     * @param array<string|null> $parts
+     *
+     * @return string
+     */
+    protected function joinParts(array $parts) : string
     {
-        return $before ? ',' . \PHP_EOL : '';
+        $result = '';
+        $hasBefore = false;
+        foreach ($parts as $part) {
+            if ($part !== null) {
+                $result .= $hasBefore ? ',' . \PHP_EOL : '';
+                $result .= $part;
+                $hasBefore = true;
+            }
+        }
+        return $result;
     }
 
     /**
--- tests/Definition/AlterTableTest.php
+++ tests/Definition/AlterTableTest.php
@@ -37,6 +37,14 @@ final class AlterTableTest extends TestCase
         $this->alterTable->sql();
     }
 
+    public function testIfExists() : void
+    {
+        self::assertSame(
+            "ALTER TABLE IF EXISTS `t1`\n ADD COLUMN `c1` int NOT NULL",
+            $this->prepare()->ifExists()->sql()
+        );
+    }
+
     public function testOptions() : void
     {
         self::assertSame(
@@ -83,6 +91,213 @@ final class AlterTableTest extends TestCase
         );
     }
 
+    public function testDropColumns() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->dropColumn('foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP COLUMN `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->dropColumn('bar', true);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP COLUMN `foo`,\n DROP COLUMN IF EXISTS `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testDropPrimaryKey() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->dropPrimaryKey();
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP PRIMARY KEY",
+            $alterTable->sql()
+        );
+    }
+
+    public function testDropKeys() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->dropKey('foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP KEY `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->dropKey('bar', true);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP KEY `foo`,\n DROP KEY IF EXISTS `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testDropForeignKeys() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->dropForeignKey('foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP FOREIGN KEY `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->dropForeignKey('bar', true);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP FOREIGN KEY `foo`,\n DROP FOREIGN KEY IF EXISTS `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testDropConstraints() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->dropConstraint('foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP CONSTRAINT `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->dropConstraint('bar', true);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DROP CONSTRAINT `foo`,\n DROP CONSTRAINT IF EXISTS `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testDisableKeys() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->disableKeys();
+        self::assertSame(
+            "ALTER TABLE `t1`\n DISABLE KEYS",
+            $alterTable->sql()
+        );
+    }
+
+    public function testEnableKeys() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->enableKeys();
+        self::assertSame(
+            "ALTER TABLE `t1`\n ENABLE KEYS",
+            $alterTable->sql()
+        );
+    }
+
+    public function testRenameTo() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->renameTo('foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n RENAME TO `foo`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testRenameColumns() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->renameColumn('c1', 'foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n RENAME COLUMN `c1` TO `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->renameColumn('c2', 'bar');
+        self::assertSame(
+            "ALTER TABLE `t1`\n RENAME COLUMN `c1` TO `foo`,\n RENAME COLUMN `c2` TO `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testRenameKeys() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->renameKey('k1', 'foo');
+        self::assertSame(
+            "ALTER TABLE `t1`\n RENAME KEY `k1` TO `foo`",
+            $alterTable->sql()
+        );
+        $alterTable->renameKey('k2', 'bar');
+        self::assertSame(
+            "ALTER TABLE `t1`\n RENAME KEY `k1` TO `foo`,\n RENAME KEY `k2` TO `bar`",
+            $alterTable->sql()
+        );
+    }
+
+    public function testConvertToCharset() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->convertToCharset('utf8mb4');
+        self::assertSame(
+            "ALTER TABLE `t1`\n CONVERT TO CHARACTER SET 'utf8mb4'",
+            $alterTable->sql()
+        );
+        $alterTable->convertToCharset('utf8', 'utf8_general_ci');
+        self::assertSame(
+            "ALTER TABLE `t1`\n CONVERT TO CHARACTER SET 'utf8' COLLATE 'utf8_general_ci'",
+            $alterTable->sql()
+        );
+    }
+
+    public function testCharset() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->charset('utf8mb4');
+        self::assertSame(
+            "ALTER TABLE `t1`\n CHARACTER SET = 'utf8mb4'",
+            $alterTable->sql()
+        );
+        $alterTable->charset(null);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DEFAULT CHARACTER SET",
+            $alterTable->sql()
+        );
+        $alterTable->charset('Default');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DEFAULT CHARACTER SET",
+            $alterTable->sql()
+        );
+    }
+
+    public function testCollate() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->collate('utf8_general_ci');
+        self::assertSame(
+            "ALTER TABLE `t1`\n COLLATE = 'utf8_general_ci'",
+            $alterTable->sql()
+        );
+        $alterTable->collate(null);
+        self::assertSame(
+            "ALTER TABLE `t1`\n DEFAULT COLLATE",
+            $alterTable->sql()
+        );
+        $alterTable->collate('Default');
+        self::assertSame(
+            "ALTER TABLE `t1`\n DEFAULT COLLATE",
+            $alterTable->sql()
+        );
+    }
+
+    public function testLock() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->lock('default');
+        self::assertSame(
+            "ALTER TABLE `t1`\n LOCK = DEFAULT",
+            $alterTable->sql()
+        );
+        $alterTable->lock('Foo');
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid LOCK value: Foo');
+        $alterTable->sql();
+    }
+
+    public function testForce() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->force();
+        self::assertSame(
+            "ALTER TABLE `t1`\n FORCE",
+            $alterTable->sql()
+        );
+    }
+
+    public function testAlgorithm() : void
+    {
+        $alterTable = $this->alterTable->table('t1')->algorithm('default');
+        self::assertSame(
+            "ALTER TABLE `t1`\n ALGORITHM = DEFAULT",
+            $alterTable->sql()
+        );
+        $alterTable->algorithm('Foo');
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid ALGORITHM value: Foo');
+        $alterTable->sql();
+    }
+
     public function testWait() : void
     {
         self::assertSame(
@@ -96,6 +311,19 @@ final class AlterTableTest extends TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Invalid WAIT value: -1');
         $this->prepare()->wait(-1)->sql();
+    }
+
+    public function testNoWait() : void
+    {
+        $alterTable = $this->prepare()->noWait();
+        self::assertSame(
+            "ALTER TABLE `t1`\n NOWAIT\n ADD COLUMN `c1` int NOT NULL",
+            $alterTable->sql()
+        );
+        $alterTable->wait(10);
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('WAIT and NOWAIT can not be used together');
+        $alterTable->sql();
     }
 
     public function testOnline() : void
@@ -119,13 +347,26 @@ final class AlterTableTest extends TestCase
         $this->createDummyData();
         $statement = $this->alterTable->table('t1')
             ->add(static function (TableDefinition $definition) : void {
-                $definition->column('c3')->varchar(100)->default('Foo Bar');
-            });
-        self::assertSame(0, $statement->run());
-        static::$database->exec('INSERT INTO `t1` SET `c1` = 123, `c2` = "z"');
+                $definition->column('foo')->varchar(100)->default('Foo Bar');
+                $definition->column('bar')->int()->null();
+            })->renameColumn('c1', 'id')->dropColumn('c2');
         self::assertSame(
-            'Foo Bar',
-            static::$database->query('SELECT * FROM `t1` WHERE `c1` = 123')->fetch()->c3
+            "ALTER TABLE `t1`\n" .
+            " ADD COLUMN `foo` varchar(100) NOT NULL DEFAULT 'Foo Bar',\n" .
+            " ADD COLUMN `bar` int NULL,\n" .
+            " DROP COLUMN `c2`,\n" .
+            ' RENAME COLUMN `c1` TO `id`',
+            $statement->sql()
+        );
+        self::assertSame(0, $statement->run());
+        static::$database->exec('INSERT INTO `t1` SET `id` = 123');
+        self::assertSame(
+            [
+                'id' => 123,
+                'foo' => 'Foo Bar',
+                'bar' => null,
+            ],
+            static::$database->query('SELECT * FROM `t1` WHERE `id` = 123')->fetchArray()
         );
     }
 }
